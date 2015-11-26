ceea388de41be1cf5caac73b08445d44d96d40ad
aldolipani <aldo@Aldos-MacBook-Pro.local>
2015-11-26 13:18:08 +0100

    App Scaffolded

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loggerBotViewerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the loggerBotViewerApp
+ */
+angular.module('loggerBotViewerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name loggerBotViewerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the loggerBotViewerApp
+ */
+angular.module('loggerBotViewerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
